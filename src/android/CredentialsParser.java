b918044c964e18d90cd241667a41e3f1893d59a8
= <fernando@soluciones3f.com.ar>
2014-12-19 02:23:24 -0300

    Working Android version.

---
+++ src/android/CredentialsParser.java
@@ -0,0 +1,26 @@
+package net.ninjaenterprises.nuance;
+import android.util.Log;
+
+public class CredentialsParser {
+	private byte[] secret;
+
+	public CredentialsParser(String secret) {
+		this.secret = hexStringToByteArray(secret);
+	}
+
+	public byte[] get() {
+		return secret;
+	}
+
+	// thanks http://stackoverflow.com/questions/140131/convert-a-string-representation-of-a-hex-dump-to-a-byte-array-using-java
+	public static byte[] hexStringToByteArray(String s) {
+	    int len = s.length();
+	    byte[] data = new byte[len / 2];
+	    for (int i = 0; i < len; i += 2) {
+	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+	                             + Character.digit(s.charAt(i+1), 16));
+	    }
+	    return data;
+}
+    
+}
